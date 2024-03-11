deedea1a56deb907f0dfebfdd90cc9657aab66a1
Alex Russell <alexrusselldev@gmail.com>
2024-03-11 11:51:15 +0000

    solution(2015-02): ✅ add solution part 2

---
+++ challenges/2015/day-2/solution-2.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(`${__dirname}/input`).toString();
+
+const inputLines = input.split("\n");
+
+const paperCalculated = inputLines.map((input) => {
+  const values = input.split("x").map((value) => parseInt(value));
+  values.sort((a, b) => {
+    return a - b;
+  });
+
+  let result = 0;
+
+  result += 2 * (values[0] + values[1]);
+
+  result += values[0] * values[1] * values[2];
+
+  return result;
+});
+
+const paperNeeded = paperCalculated.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(paperNeeded);
