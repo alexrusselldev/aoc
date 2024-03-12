294ecf550adda925f4a5680707853b23339b74a3
Alex Russell <alexrusselldev@gmail.com>
2024-03-12 09:47:55 +0000

    solution(2015-04): ✅ add solution part 2

---
+++ challenges/2015/day-4/solution-2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+const input = fs.readFileSync(`${__dirname}/input`).toString();
+
+let index = 1;
+
+while (true) {
+  const currentInput = `${input}${index}`;
+
+  const hash = crypto.createHash("md5").update(currentInput).digest("hex");
+
+  if (hash.substring(-1, 6) == "000000") {
+    break;
+  }
+
+  index++;
+}
+
+console.log(index);
